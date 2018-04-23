6f3cdc06fb8ab8e022a8cc117ded568e96cff163
nuttapon.t <nuttapon.tavee@gmail.com>
2018-04-23 18:24:11 +0700

    add

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,17 +1,14 @@
 import React from 'react'
-import { Header } from '../components'
-import { Articles, Users } from './'
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import rootReducer from '../reducers'
+import { Articles } from './';
 
 const store = createStore(rootReducer);
+
 export default () => (
   <Provider store={store}>
-    <div>
-      <Header />
-      <Articles />
-      <Users />
-    </div>
+    <Articles />
   </Provider>
 )
+
